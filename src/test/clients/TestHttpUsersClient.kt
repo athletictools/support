a0eb3c07e3a65806d4939df30c4f966fc2cfec13
John <john@Johns-MacBook-Pro.local>
2021-09-07 20:58:13 +0300

    HttpUsersClient test

---
+++ src/test/clients/TestHttpUsersClient.kt
@@ -0,0 +1,15 @@
+package clients
+import kotlinx.coroutines.runBlocking
+import support.clients.HttpUsersClient
+import support.entities.User
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class TestHttpUsersClient {
+    @Test
+    fun testGetInfo() {
+        val expectedUser = User(id = 1, fullName = "Ivanov Ivan")
+        val client = HttpUsersClient()
+        assertEquals(expectedUser, runBlocking { client.getInfo(1) })
+    }
+}
